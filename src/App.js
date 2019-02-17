a0435a3fa722349201368fd371e78bf22ddefdbf
Mateusz <matteuszd@gmail.com>
2019-02-17 19:28:58 +0100

    Add highlights component

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import "./resources/styles.css";
 import Header from "./components/header/Header";
 import Featured from "./components/featured/index";
 import VenueNFO from "./components/venueNFO/VenueNFO";
+import Highlights from "./components/highlights/Highlights";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: "300vh", background: "orangered" }}>
+      <div className="App" style={{ height: "300vh" }}>
         {" "}
         <Header />
         <Featured />
         <VenueNFO />
+        <Highlights />
       </div>
     );
   }
---
+++ src/components/highlights/Discount.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+
+import Fade from "react-reveal/Fade";
+import Slide from "react-reveal/Slide";
+
+class Discount extends Component {
+  state = {
+    discountStart: 0,
+    discountEnd: 30
+  };
+
+  handlePercentage = () => {
+    const { discountEnd, discountStart } = this.state;
+    if (discountStart < discountEnd) {
+      this.setState({
+        discountStart: discountStart + 1
+      });
+    }
+  };
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.handlePercentage();
+    }, 50);
+  }
+  render() {
+    const { discountStart } = this.state;
+    return (
+      <div className="center_wrapper">
+        <div className="discount_wrapper">
+          <Fade onReveal={() => this.handlePercentage()}>
+            {" "}
+            <div className="discount_percentage">
+              <span>{discountStart}</span>
+              <span>OFF</span>
+            </div>
+          </Fade>
+
+          <Slide right>
+            {" "}
+            <div className="discount_description ">
+              <h3>Purchase tickets before 20th JUNE</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Provident atque deserunt voluptatibus consequuntur laboriosam
+                alias. Suscipit cumque alias laudantium. Et error suscipit
+                recusandae quos! Recusandae nulla ullam doloribus aut numqua
+              </p>
+              <div>button</div>
+            </div>
+          </Slide>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Discount;
